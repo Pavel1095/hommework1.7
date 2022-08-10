6df3fa1c1fab64ce4731b2ca63578d9fad9672ee
Pavel1095 <9153220501@mail.ru>
2022-08-10 17:38:43 +0300

    Второе здание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         int year = 2022;
         Main main = new Main ();
         main.isLeapYearAndPrint(year);
+
+        int ckientOs = 0;
+        main.checkAndPrintSuitableVersionOs(ckientOs, year);
     }
 
     public void isLeapYearAndPrint(int year) {
@@ -10,6 +13,20 @@ public class Main {
             System.out.printf("%s год високосный \n", year);
         } else {
             System.out.printf("%s год не високосный \n", year);
+        }
+    }
+
+    public void checkAndPrintSuitableVersionOs ( int clientOs, int year) {
+        if (clientOs == 0 && year == 2022) {
+            System.out.println("Вам нужна современная версия для IOS");
+        }else if (clientOs == 0 && year < 2022) {
+            System.out.println("Вам нужна lite-версия для IOS");
+        }else if (clientOs == 1 && year == 2022) {
+            System.out.println("Вам нужна современная версия для Android");
+        }else if (clientOs == 1 && year < 2022) {
+            System.out.println("Вам нужна lite-версия для Android");
+        }else{
+            System.out.println("Ваша ОС не обслуживвается");
         }
     }
 }
